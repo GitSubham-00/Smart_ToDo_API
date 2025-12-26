a4a753aa4a7c0f23e3abe022aeccb476a49831f2
Subham Maity <162454635+GitSubham-00@users.noreply.github.com>
2025-12-26 13:03:53 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,15 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
+# MongoDB
 MONGODB_URI = os.getenv("MONGODB_URI")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
+
+# JWT
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+)
